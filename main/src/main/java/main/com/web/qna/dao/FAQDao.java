9a26424e2b283baf85b2770ff37f1f6d8da82cd1
yuseonjeong <yuseonjeong@localhost>
2024-05-28 04:49:12 +0900

    ysj_FAQ수정

---
+++ main/src/main/java/main/com/web/qna/dao/FAQDao.java
@@ -0,0 +1,82 @@
+package main.com.web.qna.dao;
+
+import static main.com.web.common.JDBCTemplate.close;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import main.com.web.qna.dto.FAQ;
+
+public class FAQDao {
+    private Properties sql = new Properties();
+
+    public FAQDao() {
+        String path = FAQDao.class.getResource("/sql/faq_sql.properties").getPath();
+        try (FileReader fr = new FileReader(path)) {
+            sql.load(fr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<FAQ> selectFAQAll(Connection conn, int cPage, int numPerpage) {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<FAQ> faqs = new ArrayList<>();
+
+        try {
+            pstmt = conn.prepareStatement(sql.getProperty("selectFAQAll"));
+            int start = (cPage - 1) * numPerpage + 1;
+            int end = cPage * numPerpage;
+            pstmt.setInt(1, start);
+            pstmt.setInt(2, end);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                faqs.add(getFAQ(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(rs);
+            close(pstmt);
+        }
+        return faqs;
+    }
+
+    public int selectFAQAllCount(Connection conn) {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        int result = 0;
+        try {
+            pstmt = conn.prepareStatement(sql.getProperty("selectFAQAllCount"));
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                result = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(rs);
+            close(pstmt);
+        }
+        return result;
+    }
+
+    private FAQ getFAQ(ResultSet rs) throws SQLException {
+        return FAQ.builder()
+                .faqAllNo(rs.getInt("faqAllNo"))
+                .faqCategory(rs.getString("faqCategory"))
+                .faqTitle(rs.getString("faqTitle"))
+                .faqContent(rs.getString("faqContent"))
+                .faqDate(rs.getDate("faqDate"))
+                .location(rs.getString("location"))
+                .build();
+    }
+}
---
+++ main/src/main/java/main/com/web/qna/service/FAQService.java
@@ -0,0 +1,28 @@
+package main.com.web.qna.service;
+
+import static main.com.web.common.JDBCTemplate.getConnection;
+import static main.com.web.common.JDBCTemplate.close;
+
+import java.sql.Connection;
+import java.util.List;
+
+import main.com.web.qna.dao.FAQDao;
+import main.com.web.qna.dto.FAQ;
+
+public class FAQService {
+    private FAQDao dao = new FAQDao();
+
+    public List<FAQ> selectFAQAll(int cPage, int numPerpage) {
+        Connection conn = getConnection();
+        List<FAQ> faqs = dao.selectFAQAll(conn, cPage, numPerpage);
+        close(conn);
+        return faqs;
+    }
+
+    public int selectFAQAllCount() {
+        Connection conn = getConnection();
+        int result = dao.selectFAQAllCount(conn);
+        close(conn);
+        return result;
+    }
+}
